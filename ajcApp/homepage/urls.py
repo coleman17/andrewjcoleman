78bf382b13ffcba2d2916291b9223d64f3d54943
andrewcoleman <coleman17@gmail.com>
2017-04-03 19:17:00 -0400

    Deleting all.

--- ajcApp/homepage/urls.py
+++
@@ -1,8 +0,0 @@
-# homepage/urls.py
-from django.conf.urls import url
-from homepage import views
-
-urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
-    url(r'^about/$', views.AboutPageView.as_view()), #About page route
-]
